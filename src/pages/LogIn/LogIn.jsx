68f0c12fd637a99b5d701199f616a17dffdde6ed
MD.AL-AMIN <alaminhossainmomenul87@gmail.com>
2025-07-02 15:13:17 +0600

    done

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,3 +1,223 @@
+// import React, { useRef, useContext } from "react";
+// import { NavLink, useLocation, useNavigate } from "react-router";
+// import {
+//   GoogleAuthProvider,
+//   sendPasswordResetEmail,
+//   signInWithPopup,
+// } from "firebase/auth";
+// import { auth } from "../../firebase/firebase.init";
+// import { Bounce, toast, ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import Swal from "sweetalert2";
+// import { AuthContext } from "../../context/AuthContext/AuthContext";
+
+// const LogIn = () => {
+//   const { loginUser } = useContext(AuthContext);
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   const provider = new GoogleAuthProvider();
+//   const emailRef = useRef();
+
+//   // Get Firebase ID token from current user
+//   const getIdToken = async () => {
+//     const user = auth.currentUser;
+//     if (user) {
+//       return await user.getIdToken(true);
+//     }
+//     return null;
+//   };
+
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+//     const email = e.target.email.value;
+//     const password = e.target.password.value;
+
+//     loginUser(email, password)
+//       .then(async (result) => {
+//         const token = await getIdToken();
+//         if (!token) throw new Error("Failed to get ID token");
+//         // ✅ Store the token
+//         localStorage.setItem("auth-token", token);
+
+//         const signInInfo = {
+//           email,
+//           lastSignInTime: result.user.metadata.lastSignInTime,
+//         };
+
+//         fetch("https://server-site-alpha-umber.vercel.app/users", {
+//           method: "PATCH",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//           body: JSON.stringify(signInInfo),
+//         })
+//           .then((res) => {
+//             if (!res.ok) {
+//               throw new Error("Failed to update last sign-in time");
+//             }
+//             return res.json();
+//           })
+//           .then(() => {
+//             toast.success("Login Successful", {
+//               position: "top-right",
+//               autoClose: 5000,
+//               theme: "light",
+//               transition: Bounce,
+//             });
+//             navigate(location?.state || "/");
+//           })
+//           .catch((err) => {
+//             toast.error(err.message);
+//           });
+//       })
+//       .catch((error) => {
+//         toast.error(`Login failed: ${error.message}`);
+//       });
+//   };
+
+//   const handleGoogleLogin = () => {
+//     signInWithPopup(auth, provider)
+//       .then(async (result) => {
+//         const user = result.user;
+//         const token = await getIdToken();
+//         if (!token) throw new Error("Failed to get ID token");
+
+//         const userProfile = {
+//           name: user.displayName,
+//           email: user.email,
+//           creationTime: user.metadata.creationTime,
+//           lastSignInTime: user.metadata.lastSignInTime,
+//         };
+
+//         fetch(`https://server-site-alpha-umber.vercel.app/users?email=${user.email}`, {
+//           headers: { Authorization: `Bearer ${token}` },
+//         })
+//           .then((res) => res.json())
+//           .then(async (existingUsers) => {
+//             if (existingUsers?.length > 0) {
+//               toast.info("Welcome back!");
+//               navigate(location?.state || "/");
+//             } else {
+//               const postRes = await fetch("https://server-site-alpha-umber.vercel.app/users", {
+//                 method: "POST",
+//                 headers: {
+//                   "Content-Type": "application/json",
+//                   Authorization: `Bearer ${token}`,
+//                 },
+//                 body: JSON.stringify(userProfile),
+//               });
+//               if (!postRes.ok) {
+//                 throw new Error("Failed to create user");
+//               }
+//               const postData = await postRes.json();
+//               if (postData.insertedId) {
+//                 Swal.fire({
+//                   position: "top-end",
+//                   icon: "success",
+//                   title: "Your Profile has been created",
+//                   showConfirmButton: false,
+//                   timer: 1500,
+//                 });
+//               }
+//               navigate(location?.state || "/");
+//             }
+//           })
+//           .catch((err) => {
+//             toast.error("Failed to verify user or create profile");
+//             console.error(err);
+//           });
+//       })
+//       .catch((error) => {
+//         toast.error(`Google login failed: ${error.message}`);
+//       });
+//   };
+
+//   const handleForgetPassword = () => {
+//     const email = emailRef.current?.value;
+//     if (!email) {
+//       toast.warn("Please enter your email first.");
+//       return;
+//     }
+
+//     sendPasswordResetEmail(auth, email)
+//       .then(() => {
+//         toast.success("Password reset email sent!");
+//       })
+//       .catch((error) => {
+//         toast.error(`Failed to send reset email: ${error.message}`);
+//       });
+//   };
+
+//   return (
+//     <>
+//       <ToastContainer />
+//       <div className="hero min-h-screen bg-base-200 px-4 mt-0">
+//         <div className="hero-content flex-col gap-10 w-full max-w-6xl">
+//           <div className="text-center lg:w-1/2">
+//             <h1 className="text-4xl lg:text-5xl font-bold">Login now!</h1>
+//             <p className="pt-6 text-base lg:text-lg max-w-md mx-auto">
+//               Log in to share meals, discover local dishes, and connect with
+//               food lovers in your community. Your next meaningful bite starts
+//               here.
+//             </p>
+//           </div>
+
+//           <div className="card w-full max-w-sm bg-base-100 shadow-2xl lg:w-1/2">
+//             <div className="card-body">
+//               <form onSubmit={handleLogin}>
+//                 <label className="label">Email</label>
+//                 <input
+//                   name="email"
+//                   type="email"
+//                   className="input input-bordered w-full"
+//                   placeholder="Email"
+//                   ref={emailRef}
+//                   required
+//                 />
+//                 <label className="label mt-2">Password</label>
+//                 <input
+//                   name="password"
+//                   type="password"
+//                   className="input input-bordered w-full"
+//                   placeholder="Password"
+//                   required
+//                 />
+//                 <div className="text-right mt-2">
+//                   <button
+//                     type="button"
+//                     onClick={handleForgetPassword}
+//                     className="link link-hover text-sm text-blue-500"
+//                   >
+//                     Forgot password?
+//                   </button>
+//                 </div>
+//                 <button className="btn btn-neutral mt-4 w-full">Login</button>
+//               </form>
+
+//               <p className="text-sm text-center mt-4">
+//                 Don't have an account?{" "}
+//                 <NavLink className="text-blue-500 underline" to="/register">
+//                   Register
+//                 </NavLink>
+//               </p>
+
+//               <button
+//                 onClick={handleGoogleLogin}
+//                 className="btn bg-white text-black border border-gray-300 mt-4 w-full"
+//               >
+//                 Login with Google
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default LogIn;
+
 import React, { useRef, useContext } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router";
 import {
@@ -18,27 +238,13 @@ const LogIn = () => {
   const provider = new GoogleAuthProvider();
   const emailRef = useRef();
 
-  // Get Firebase ID token from current user
-  const getIdToken = async () => {
-    const user = auth.currentUser;
-    if (user) {
-      return await user.getIdToken(true);
-    }
-    return null;
-  };
-
   const handleLogin = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
 
     loginUser(email, password)
-      .then(async (result) => {
-        const token = await getIdToken();
-        if (!token) throw new Error("Failed to get ID token");
-        // ✅ Store the token
-        localStorage.setItem("auth-token", token);
-
+      .then((result) => {
         const signInInfo = {
           email,
           lastSignInTime: result.user.metadata.lastSignInTime,
@@ -48,7 +254,6 @@ const LogIn = () => {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify(signInInfo),
         })
@@ -80,8 +285,6 @@ const LogIn = () => {
     signInWithPopup(auth, provider)
       .then(async (result) => {
         const user = result.user;
-        const token = await getIdToken();
-        if (!token) throw new Error("Failed to get ID token");
 
         const userProfile = {
           name: user.displayName,
@@ -90,23 +293,25 @@ const LogIn = () => {
           lastSignInTime: user.metadata.lastSignInTime,
         };
 
-        fetch(`https://server-site-alpha-umber.vercel.app/users?email=${user.email}`, {
-          headers: { Authorization: `Bearer ${token}` },
-        })
+        fetch(
+          `https://server-site-alpha-umber.vercel.app/users?email=${user.email}`
+        )
           .then((res) => res.json())
           .then(async (existingUsers) => {
             if (existingUsers?.length > 0) {
               toast.info("Welcome back!");
               navigate(location?.state || "/");
             } else {
-              const postRes = await fetch("https://server-site-alpha-umber.vercel.app/users", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(userProfile),
-              });
+              const postRes = await fetch(
+                "https://server-site-alpha-umber.vercel.app/users",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(userProfile),
+                }
+              );
               if (!postRes.ok) {
                 throw new Error("Failed to create user");
               }
@@ -217,5 +422,3 @@ const LogIn = () => {
 };
 
 export default LogIn;
-
-
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,3 +1,209 @@
+// import React, { useContext } from "react";
+// import { NavLink, useLocation, useNavigate } from "react-router";
+// import { toast } from "react-toastify";
+// import { auth } from "../../firebase/firebase.init";
+// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import Swal from "sweetalert2";
+// import { AuthContext } from "../../context/AuthContext/AuthContext";
+
+// const SignUp = () => {
+//   const { createUser } = useContext(AuthContext);
+//   const navigate = useNavigate();
+//   const provider = new GoogleAuthProvider();
+//   const location = useLocation();
+
+//   const handleRegister = async (e) => {
+//     e.preventDefault();
+//     const form = e.target;
+//     const formData = new FormData(form);
+//     const { email, password, ...rest } = Object.fromEntries(formData.entries());
+
+//     if (!email || !password) {
+//       return toast.error("Email and password are required");
+//     }
+
+//     try {
+//       const result = await createUser(email, password);
+//       const user = result.user;
+//       const token = await user.getIdToken();
+
+//       const userProfile = {
+//         email,
+//         ...rest,
+//         creationTime: user.metadata?.creationTime,
+//         lastSignInTime: user.metadata?.lastSignInTime,
+//       };
+
+//       const res = await fetch(`https://server-site-alpha-umber.vercel.app/users`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         body: JSON.stringify(userProfile),
+//       });
+
+//       const data = await res.json();
+//       if (res.ok && data.insertedId) {
+//         Swal.fire({
+//           position: "top-end",
+//           icon: "success",
+//           title: "Your Profile has been created",
+//           showConfirmButton: false,
+//           timer: 1500,
+//         });
+//         navigate("/login");
+//       } else {
+//         toast.error(data.message || "Signup failed");
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       toast.error(error.message || "Signup failed");
+//     }
+//   };
+
+//   const handleGoogleLogin = async () => {
+//     try {
+//       const result = await signInWithPopup(auth, provider);
+//       const user = result.user;
+//       const token = await user.getIdToken();
+
+//       const userProfile = {
+//         name: user.displayName,
+//         email: user.email,
+//         photoURL: user.photoURL,
+//         creationTime: user.metadata?.creationTime,
+//         lastSignInTime: user.metadata?.lastSignInTime,
+//       };
+
+//       const res = await fetch(`https://server-site-alpha-umber.vercel.app/users`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         body: JSON.stringify(userProfile),
+//       });
+
+//       const data = await res.json();
+
+//       if (res.ok && data.insertedId) {
+//         Swal.fire({
+//           position: "top-end",
+//           icon: "success",
+//           title: "Your Profile has been created",
+//           showConfirmButton: false,
+//           timer: 1500,
+//         });
+//       }
+
+//       toast.success("Logged in with Google!");
+//       navigate(location?.state?.from || "/");
+//     } catch (error) {
+//       toast.error(`Google login failed: ${error.message}`);
+//     }
+//   };
+
+//   return (
+//     <div className="hero min-h-screen bg-base-200 px-4">
+//       <div className="hero-content flex-col justify-center items-center gap-10 w-full max-w-6xl">
+//         <div className="text-center  lg:w-1/2">
+//           <h1 className="text-4xl lg:text-5xl font-bold">Register now!</h1>
+//           <p className="pt-6 text-base lg:text-lg max-w-md mx-auto">
+//             Reduce food waste, share extra meals, and connect with a community
+//             that cares. Start making a difference—one dish at a time.
+//           </p>
+//         </div>
+
+//         <div className="card w-full max-w-sm bg-base-100 shadow-2xl lg:w-1/2">
+//           <div className="card-body">
+//             <form onSubmit={handleRegister}>
+//               <label className="label">Name</label>
+//               <input
+//                 name="name"
+//                 type="text"
+//                 className="input input-bordered w-full"
+//                 placeholder="Name"
+//               />
+//               <label className="label">Photo URL</label>
+//               <input
+//                 name="photoURL"
+//                 type="text"
+//                 className="input input-bordered w-full"
+//                 placeholder="Photo URL"
+//                 required
+//               />
+//               <label className="label mt-2">Email</label>
+//               <input
+//                 name="email"
+//                 type="email"
+//                 className="input input-bordered w-full"
+//                 placeholder="Email"
+//                 required
+//               />
+//               <label className="label mt-2">Password</label>
+//               <input
+//                 name="password"
+//                 type="password"
+//                 className="input input-bordered w-full"
+//                 placeholder="Password"
+//                 required
+//               />
+//               <button className="btn btn-neutral mt-4 w-full" type="submit">
+//                 Register
+//               </button>
+//             </form>
+
+//             <p className="text-sm text-center mt-4">
+//               Already have an account?{" "}
+//               <NavLink className="text-blue-500 underline" to="/signIn">
+//                 Login
+//               </NavLink>
+//             </p>
+//             <button
+//               onClick={handleGoogleLogin}
+//               className="btn bg-white text-black border border-gray-300 mt-4 w-full"
+//             >
+//               {/* Google SVG icon */}
+//               <svg
+//                 aria-label="Google logo"
+//                 width="16"
+//                 height="16"
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 viewBox="0 0 512 512"
+//                 className="mr-2"
+//               >
+//                 <g>
+//                   <path d="m0 0H512V512H0" fill="#fff"></path>
+//                   <path
+//                     fill="#34a853"
+//                     d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+//                   ></path>
+//                   <path
+//                     fill="#4285f4"
+//                     d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+//                   ></path>
+//                   <path
+//                     fill="#fbbc02"
+//                     d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+//                   ></path>
+//                   <path
+//                     fill="#ea4335"
+//                     d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+//                   ></path>
+//                 </g>
+//               </svg>
+//               Login with Google
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignUp;
+
 import React, { useContext } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router";
 import { toast } from "react-toastify";
@@ -25,7 +231,6 @@ const SignUp = () => {
     try {
       const result = await createUser(email, password);
       const user = result.user;
-      const token = await user.getIdToken();
 
       const userProfile = {
         email,
@@ -38,7 +243,6 @@ const SignUp = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(userProfile),
       });
@@ -52,7 +256,7 @@ const SignUp = () => {
           showConfirmButton: false,
           timer: 1500,
         });
-        navigate("/login");
+        navigate("/signIn");
       } else {
         toast.error(data.message || "Signup failed");
       }
@@ -66,7 +270,6 @@ const SignUp = () => {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
-      const token = await user.getIdToken();
 
       const userProfile = {
         name: user.displayName,
@@ -80,7 +283,6 @@ const SignUp = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(userProfile),
       });
@@ -160,11 +362,11 @@ const SignUp = () => {
                 Login
               </NavLink>
             </p>
+
             <button
               onClick={handleGoogleLogin}
               className="btn bg-white text-black border border-gray-300 mt-4 w-full"
             >
-              {/* Google SVG icon */}
               <svg
                 aria-label="Google logo"
                 width="16"
@@ -203,3 +405,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
